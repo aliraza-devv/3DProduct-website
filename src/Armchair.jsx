c8d9494df218766a042d02ddb22042ba66b1a8c5
Ali Raza <aliraza.dev.web@gmail.com>
2023-11-26 21:52:52 +0500

    added material for chair

--- src/Armchair.jsx
+++ src/Armchair.jsx
@@ -17,8 +17,12 @@ export function ArmChair(props) {
   return (
     <group {...props} dispose={null}>
       <group  rotation={[ -1.67, 0, -2.4]} scale={0.001}>
-        <mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials.Lounger_wire_115115115} />
-        <mesh castShadow receiveShadow geometry={nodes.Object_3.geometry} material={materials.Lounger_wire_115115115} />
+        <mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials.Lounger_wire_115115115} >
+        <meshStandardMaterial color="#d4a373" />
+        </mesh>
+        <mesh castShadow receiveShadow geometry={nodes.Object_3.geometry} material={materials.Lounger_wire_115115115} >
+        <meshStandardMaterial color="#d4a373" />
+        </mesh>
       </group>
     </group>
   )
